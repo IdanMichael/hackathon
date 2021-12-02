20e841f6af26630089a2e972f52b66ef1a4c2f8a
IdanMichael <77175647+IdanMichael@users.noreply.github.com>
2021-12-01 19:23:02 -0500

    added js file

--- main.js
+++ main.js
@@ -1,2 +1,7 @@
 //fetch dislike count for a given youtube video
 //insert dislike count into youtube html
+
+document.addEventListener('DOMContentLoaded', () => {
+  const dislike = document.querySelectorAll(".style-scope ytd-toggle-button-renderer style-text")
+  console.log("test id:" , dislike)
+});
